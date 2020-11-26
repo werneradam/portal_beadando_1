29a3aa188be3a901e0445891027d06b549973f9b
Prehoda Barna <prehodab3@gmail.com>
2020-11-26 18:22:26 +0100

    Fix check and login pages

--- check.php
+++ check.php
@@ -30,31 +30,4 @@
         header("Location: registration.php");
         exit;
     }
-    //Bejelentkezési adatok ellenőrzése
-    if(isset($_POST["login"])){
-        $password_hashed = sha1(mysqli_real_escape_string($kapcsolat,$_POST["password"]));
-        $email = mysqli_real_escape_string($kapcsolat,$_POST["email"]);
-
-        $sql = "SELECT email, password FROM Beadando1.Users WHERE email = '$email' AND password = '$password_hashed'";
-        $result = mysqli_query($kapcsolat,$sql);
-        if(mysqli_num_rows($result)==1){
-
-            session_start();
-            $_SESSION["username"] = $username;
-            $_SESSION["loggedin"] = true;
-
-            $user_data = mysqli_fetch_assoc($result);
-            if($user_data["is_admin"]==1){
-
-                $_SESSION["is_admin"] = 1;
-            }else{
-                $_SESSION["is_admin"] = 0;
-            }
-            //Itt kéne átvinni a főoldalunkra
-            //header("Location: registration.php");
-        }
-
-    } else{
-        header("Location: registration.php");
-        exit;
-    }
+?>
--- login.php
+++ login.php
@@ -23,8 +23,8 @@
         }
         ?>
     </div>
-
-    <form action="check.php" method="POST">
+    
+    <form action="login.php" method="POST">
         <table>
             <tr>
                 <td>Email-cím:</td>
@@ -46,6 +46,34 @@
             </tr>
         </table>
     </form>
+    <?php
+        if(isset($_POST["login"])){
+            require_once("connect.php");
+            $password_hashed = sha1(mysqli_real_escape_string($kapcsolat,$_POST["password"]));
+            $email = mysqli_real_escape_string($kapcsolat,$_POST["email"]);
+            $sql = "SELECT email, password FROM Beadando1.Users WHERE email = '$email' AND password = '$password_hashed'";
+            $result = mysqli_query($kapcsolat,$sql);
+            if(mysqli_num_rows($result)==1){
+    
+                session_start();
+                $_SESSION["username"] = $username;
+                $_SESSION["loggedin"] = true;
+    
+                $user_data = mysqli_fetch_assoc($result);
+                if($user_data["is_admin"]==1){
+    
+                    $_SESSION["is_admin"] = 1;
+                }else{
+                    $_SESSION["is_admin"] = 0;
+                }
+                //Itt kéne átvinni a főoldalunkra
+                header("Location: admin.php");
+            }else{
+                //Itt is jól jönne a formázás
+                echo"Hibás email-cím vagy jelszó!";
+            }
+        }
+    ?>
     <h5>Ha még nem regisztráltál, itt megteheted!</h5>
     <form action="registration.php"><input type="submit" name="registration" value="Regisztráció"></form>
 </body>
