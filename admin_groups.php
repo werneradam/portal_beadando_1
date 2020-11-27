1206da7e2f8c07d51ea3b2f640892941a1b2f234
dalmaszekely <dalmaszekely@users.noreply.github.com>
2020-11-28 00:51:48 +0100

    admin_groups design

--- admin_groups.php
+++ admin_groups.php
@@ -21,61 +21,66 @@ if ($_SESSION["is_admin"] == 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="img/favicon.png">
+    <link rel="stylesheet" href="css/admin_groups.css">
     <link rel="stylesheet" href="css/all.css">
     <title>Secret Santa</title>
 </head>
 
 <body class="body">
-    <!--Navigációs gomb -->
-    <form action="admin.php" method="POST"><input type="submit" name="groups" value="Felhasználók"></form>
+    <div class="header">
+        <form action="group.php" method="POST"><input type="submit" name="group" value="Csoport"  class="menu"></form>
+        <form action="admin.php" method="POST"><input type="submit" name="admin" value="Admin felület"  class="menu"></form>
+        <form action="settings.php" method="POST"><input type="submit" name="settings" value="Beállítások" class="menu"></form>
+    </div>
 
     <?php
     require_once("connect.php");
     ?>
+    <div class="box">
+        <p class="title">Csoportok és tagjaik listája</p>
+        <?php
+            //Felhasználó kirúgása a csoportból
+            if (isset($_POST["delete_from_group"])) {
+                $sql1 = "UPDATE users SET group_fk=0,is_creator=0 WHERE userid=". $_POST["userid"];
+                $query1 = mysqli_query($conn, $sql1);
+            }
+        $sql = "SELECT groups.group_id, groups.group_name, users.username, users.is_creator, users.email, users.userid, groups.event_date FROM users
+        JOIN groups
+        ON groups.group_id = users.group_fk
+        ORDER BY groups.group_id;";
+        $query = mysqli_query($conn, $sql);
 
-    <h2>Csoposrtok és tagjaik listája</h2>
-    <?php
-        //Felhasználó kirúgása a csoportból
-        if (isset($_POST["delete_from_group"])) {
-            $sql1 = "UPDATE users SET group_fk=0,is_creator=0 WHERE userid=". $_POST["userid"];
-            $query1 = mysqli_query($conn, $sql1);
-        }
-    $sql = "SELECT groups.group_id, groups.group_name, users.username, users.is_creator, users.email, users.userid, groups.event_date FROM users
-    JOIN groups
-    ON groups.group_id = users.group_fk
-    ORDER BY groups.group_id;";
-    $query = mysqli_query($conn, $sql);
+        
 
-    
-
-    ?>
-    <table>
-        <tr>
-            <td>Csoport azonosító</td>
-            <td>Csoport név</td>
-            <td>Húzás dátuma</td>
-            <td>Felhasználó neve</td>
-            <td>Felhasználó email-címe</td>
-            <td>Csoportvezető-e</td>
-        </tr>
-        <?php while ($row = mysqli_fetch_assoc($query)) { ?>
-            <tr><td><?php echo $row["group_id"] ?></td>
-                <td><?php echo $row["group_name"] ?></td>
-                <td><?php echo $row["event_date"] ?></td>
-                <td><?php echo $row["username"] ?></td>
-                <td><?php echo $row["email"] ?></td>
-                <td><?php if ($row["is_creator"] == 0) {
-                        echo "Nem";
-                    } else {
-                        echo "Igen";
-                    } ?></td>
-                <td><?php echo '<form action="admin_groups.php" method="POST">
-                <input type ="hidden" name="userid" value =' . $row["userid"] . '>
-                <input type="submit" name="delete_from_group" value="Kirúgás a csoportból">' ?>
-                    </form>
+        ?>
+        <table>
+            <tr>
+                <td class="bold">Csoport azonosító</td>
+                <td class="bold">Csoport név</td>
+                <td class="bold">Húzás dátuma</td>
+                <td class="bold">Felhasználó neve</td>
+                <td class="bold">Felhasználó email-címe</td>
+                <td class="bold">Csoportvezető-e</td>
             </tr>
-        <?php } ?>
-    </table>
+            <?php while ($row = mysqli_fetch_assoc($query)) { ?>
+                <tr><td><?php echo $row["group_id"] ?></td>
+                    <td><?php echo $row["group_name"] ?></td>
+                    <td><?php echo $row["event_date"] ?></td>
+                    <td><?php echo $row["username"] ?></td>
+                    <td><?php echo $row["email"] ?></td>
+                    <td><?php if ($row["is_creator"] == 0) {
+                            echo "Nem";
+                        } else {
+                            echo "Igen";
+                        } ?></td>
+                    <td><?php echo '<form action="admin_groups.php" method="POST">
+                    <input type ="hidden" name="userid" value =' . $row["userid"] . '>
+                    <input type="submit" name="delete_from_group" value="Kirúgás a csoportból" class="button">' ?>
+                        </form>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 </body>
 
 </html>
